f503725b8f81a2e69d34e656f6e9311cdc744459
MTMangueze <117755457+MTMangueze@users.noreply.github.com>
2022-12-08 22:51:13 +0200

    fd df

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+
 /**
-*main - Main function
+* main - Prints "with proper grammar, but the ouptu is a piece of art,"
 *
-*
-*Return: return 0 everytime
+* Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}
