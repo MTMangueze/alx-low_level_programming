9492cef001293e51f2ff5522dd35e27cfaeeb41a
MTMangueze <117755457+MTMangueze@users.noreply.github.com>
2022-12-08 22:48:36 +0200

    evdf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
-
-
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+*main - Main function
+*
+*
+*Return: return 0 everytime
+*/
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
